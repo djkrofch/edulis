0ef42c6d6387882d3c92b5cabf01f3024389a150
Dan Krofcheck <krofcheck@gmail.com>
2015-02-24 15:56:11 -0700

    Finished building functions for all Penman Monteith related aerodynamic terms, and appending them to the daily files. Most everything looks good, just need to work on setting up a filter for the new variables prior to output as csv for plotting and analysis

--- makeDailyFiles_PM.R
+++ makeDailyFiles_PM.R
@@ -8,14 +8,11 @@
 ### Email: djkrofch@unm.edu
 ### Date Modified: 23 - Feb - 2015
 
-library(numDeriv)
-
-
-
 ### ---------------- Input Data -------------------------- ###
 
 # First specify the directory where the data are located
 dataDir <- '/home/nikko/Research/Data/TowerData/AmerifluxFiles/'
+outDir <- '/home/nikko/Research/Data/TowerData/AmerifluxFiles/Products/'
 
 # Specify whether you want to look at gapfilled or with_gaps files
 # Here, we are just specifying a pattern that exists in the file names which
@@ -33,6 +30,7 @@ LV_WATER <- 2.257e6;      # J Kg-1
 CP_DRY_AIR <- 1006;   # J Kg-1 K-1
 STEFAN_BOLTZMANN_CONST <- 5.670373e-8; #J s-1 m−2 K−4
 GRAMS_C_PER_MICROEINSTEIN<- 12.01 * SECONDS_PER_HOUR / 1000000 # gC per 30 minute flux interval
+PRIESTLY_TAYLOR_COEFF <- 1.74
 Rd <- 286.9 # dry air gas const; J Kg-1 K-1
 Rw <- 461.5; # water vapor gas const; J Kg-1 K-1
 
@@ -46,8 +44,10 @@ Rw <- 461.5; # water vapor gas const; J Kg-1 K-1
 # Also need to move these function definitions from this file into another
 # script, and included as a package --- on the TO DO list.
 #
-# These functions all come from joint work by Tim Hilton, and Andy Fox
+# These functions represent work by Dan Krofcheck, Tim Hilton, and Andy Fox.
+# for the University of New Mexico, Biology Dept.
 
+# T must be in Kelvin
 get_es <- function( T ){
 T_s <- 373.16 # steam_point temperature (K)
 log_10_e <- ( -7.90298 * ( ( T_s/ T ) - 1 ) ) + 
@@ -61,20 +61,25 @@ return(e_s)
 
 calc_albedo <- function(ROut, RIn){
 albedo <- ROut / RIn
+print('calculating albedo')
 return(albedo)
 }
 
 calc_emissivity <- function(albedo){
 epsilon <- -0.16 * albedo + 0.99
+print('calculating emissivity')
 return(epsilon)
 }
 
-calc_Ts <- function(epsilon, RLong_Out){
-Ts <- (RLong_out / (STEFAN_BOLTZMANN_CONST * epsilon)) ^ (0.25)
+calc_Ts <- function(epsilon, Rlong_out){
+Ts <- (Rlong_out / (STEFAN_BOLTZMANN_CONST * epsilon)) ^ (0.25)
 Ts <- Ts - 273.15 # Convert K to degC
+print('calculating surface temperature')
 return(Ts)
 }
 
+# Moist air density calculation.
+# T must be in Kelvin, p in Pascals, and RH in ratio (not %)
 calc_moist_air_density <- function( T, p, RH ){
 epsilon <- Rd / Rw
 w <- (calc_ws(T, p) / 100 ) * RH # Mixing ratio of water vapor
@@ -83,6 +88,7 @@ rho <- (p / (Rd * T)) * (1 - (( e / p) * (1 - epsilon))) # Wallace & Hobbs eqs 2
 return(rho)
 }
 
+# T must be in Kelvin, p in Pascals (not kPa)
 calc_ws <- function(T, p ){
 epsilon <- Rd / Rw
 e_s <- get_es(T)
@@ -90,47 +96,65 @@ w_s <- epsilon * (e_s / (p - e_s)) # Wallace & Hobbs eqs 2.64 and 2.14
 return(w_s)
 }
 
+# T must be in Kelvin, p in Pascals, and RH in ratio (not %)
 calc_cp <- function(T, p, RH){
 w_s <- calc_ws( T, p ) # saturation water vapor mixing ratio
-w <- ( RH / 100 ) * w_s # water vator mixing ratio, Wallace & Hobbs eq. 2.66
+w <- ( RH ) * w_s # water vator mixing ratio, Wallace & Hobbs eq. 2.66
 c_p <- CP_DRY_AIR * ( 1.0 + ( 0.84 * w ) ); # Stull eq. 3.2
 return(c_p)
 }
 
+# T_a is in degrees Celcius
 calc_delta <- function(T_a){
 #T_ref <- ( -50:1:100 ) + 273.15  # -50C to 100C converted to K
 #es_kPa <- get_es( T_ref ) / 10.0 # saturation vapor pressure in kPa
-#des_dt <- grad( es_kPa , T_ref )
 #delta <- approx( T_ref, des_dt, T_a + 273.15 )
 slopeSAT <- (2508.3 / (T_a + 237.3)^2) * exp(17.3 * T_a / (T_a + 237.3)) # temporary calc
 return(slopeSAT)
 }
 
 calc_ga <- function(T_a, p, RH, H, T_s){
-c_p =1024;
-c_p = calc_cp( T_a + 273.15, P, RH );
+#c_p =1024;
+c_p = calc_cp( T_a + 273.15, p, RH );
 ga = H / ( rho * c_p * ( T_s - T_a ) )
 return(ga)
 }
 
 calc_eta <- function(T_a, p, RH){
 c_p <- calc_cp( T_a + 273.15, p, RH )
-c_p <- 1024;
+#c_p <- 1024;
 gamma <- c_p / LV_WATER; # pyschrometric constant
 delta <- calc_delta( T_a )
-rho <- calc_moist_air_density(T_a, p, RH)
+rho <- calc_moist_air_density(T_a + 273.15, p, RH)
 eta <- ( rho * c_p * ga ) / 
-      ( 1 - ( obj.alpha_PT * ( delta / ( delta  + gamma ) ) ) )
+      ( 1 - ( PRIESTLY_TAYLOR_COEFF * ( delta / ( delta  + gamma ) ) ) )
 return(eta)
+}
+
+calc_gc <- function(T,  p, RH, VPD, H, LE){
+epsilon = Rd / Rw
+p_kPa = p/1000
+gamma = (c_p  * p_kPa) / ( LV_WATER * epsilon ) 
+bowen = H / LE
+term1 = ( ( ( delta / gamma ) * bowen ) - 1 ) * ( 1.0 / ga );
+term2 = ( ( 1012 * rho ) / ( LE * gamma ) ) * VPD;
+gc = ( 1.0 / ( term1 + term2 ) );
+R_star = 8.3144621 # uiversal gas const
+mol_2_umol = 1000.0  # factor to convert mol to umol
+T_a_K = T + 273.15  # convert C to K
+conv_factor = ( p / ( R_star * T_a_K ) ) * mol_2_umol
+gc = gc * conv_factor  # convert gc from m/s to mmol/m2/s
+return(gc)
 }
 
 
 # Specify the column names we want to appear in the daily file
-# Currently, edits here require edits inside the dailyfile
+# Currently, edits here require edits inside the dailyfile loop.
 # Need to have the loop dynamically generated via a function, using this list as inputs ultimately.
 dailynames <- c('SITE','YEAR','DOY','TA_mean','TA_min','TA_max','FC','FC_day','FC_night','H','LE','PRECIP',
 	'RH','PA','VPD_day','VPD_day_min','VPD_day_max','RNET','PAR','Rg','Rg_out','Rlong_in','Rlong_out',
-	'RE','GPP','GAP_qual','LUE','WUE', 'ET','PET')
+	'RE','GPP','GAP_qual','LUE','WUE', 'ET','PET', 'es','ws','albedo','emissivity','surface_temp','delta',
+	'rho','eta','cp','ga','gc')
 
 ### ------------------------------------------------------------ ###
 
@@ -195,6 +219,7 @@ for(i in 1:length(fileList)){
 	daytime <- thisday[which(thisday$Rg > 10), ]
 	nighttime <- thisday[which(thisday$Rg <= 11), ]
 	day_obs <- length(daytime$DOY)
+	PM_interval <- thisday[which(thisday$HRMIN > 930 & thisday$HRMIN < 1630),]
 
 	dailyfile[d,1] <- sitename 
 	dailyfile[d,2] <- thisday$YEAR[1]
@@ -215,10 +240,10 @@ for(i in 1:length(fileList)){
 	dailyfile[d,17] <- max(daytime$VPD, na.rm = TRUE)
 	dailyfile[d,18] <- sum(thisday$RNET, na.rm = TRUE)
 	dailyfile[d,19] <- sum(thisday$PAR, na.rm = TRUE)
-	dailyfile[d,20] <- sum(thisday$Rg, na.rm = TRUE)
-	dailyfile[d,21] <- sum(thisday$Rg_out, na.rm = TRUE)
-	dailyfile[d,22] <- sum(thisday$Rlong_in, na.rm = TRUE)
-	dailyfile[d,23] <- sum(thisday$Rlong_out, na.rm = TRUE)
+	dailyfile[d,20] <- mean(thisday$Rg, na.rm = TRUE)
+	dailyfile[d,21] <- mean(thisday$Rg_out, na.rm = TRUE)
+	dailyfile[d,22] <- mean(thisday$Rlong_in, na.rm = TRUE)
+	dailyfile[d,23] <- mean(thisday$Rlong_out, na.rm = TRUE)
 	dailyfile[d,24] <- sum(thisday$RE * GRAMS_C_PER_MICROEINSTEIN, na.rm = TRUE)
 	dailyfile[d,25] <- sum(thisday$GPP * GRAMS_C_PER_MICROEINSTEIN, na.rm = TRUE)
 	dailyfile[d,26] <- sum(thisday$FC_flag) / 48
@@ -246,6 +271,7 @@ for(i in 1:length(fileList)){
 
 	# LE potential and ET potential (from Priestly-Taylor). Note here, Rn-G is substituted with H + LE
 	LEpot = alphaPT *(slopeSAT* (H + LE) / (slopeSAT + PSI))
+
 	ETpot = LEpot * 1800 * day_obs / (1000 * lambda)
 	
 	# Write the ET related variables to the daily file
@@ -253,6 +279,31 @@ for(i in 1:length(fileList)){
 	dailyfile[d,28] <- sum(thisday$GPP * GRAMS_C_PER_MICROEINSTEIN) / ET 
 	dailyfile[d,29] <- ET
 	dailyfile[d,30] <- ETpot
+
+	es <- get_es(PM_interval$TA + 273.15)
+	ws <- calc_ws(PM_interval$TA + 273.15, PM_interval$PA * 1000)
+	albedo <- calc_albedo(PM_interval$Rg_out, PM_interval$Rg)
+	epsilon <- calc_emissivity(albedo)
+	Ts <- calc_Ts(epsilon, PM_interval$Rlong_out)
+	delta <- calc_delta(PM_interval$TA)
+	rho <- calc_moist_air_density(PM_interval$TA + 273.15, PM_interval$PA * 1000, PM_interval$RH)
+	eta <- calc_eta(PM_interval$TA, PM_interval$PA * 1000, PM_interval$RH)
+	c_p <- calc_cp(PM_interval$TA + 273.15, PM_interval$PA * 1000, PM_interval$RH)
+	ga <- calc_ga(PM_interval$TA, PM_interval$PA * 1000, PM_interval$RH, PM_interval$H, Ts)
+	gc <- calc_gc(PM_interval$TA, PM_interval$PA * 1000, PM_interval$RH, PM_interval$VPD, PM_interval$H, Ts)
+
+	dailyfile[d,31] <- mean(es, na.rm = TRUE)
+	dailyfile[d,32] <- mean(ws, na.rm = TRUE)
+	dailyfile[d,33] <- mean(albedo, na.rm = TRUE)
+	dailyfile[d,34] <- mean(epsilon, na.rm = TRUE) 
+	dailyfile[d,35] <- mean(Ts, na.rm = TRUE) 
+	dailyfile[d,36] <- mean(delta, na.rm = TRUE) 
+	dailyfile[d,37] <- mean(rho, na.rm = TRUE) 
+	dailyfile[d,38] <- mean(eta, na.rm = TRUE) 
+	dailyfile[d,39] <- mean(c_p, na.rm = TRUE) 
+	dailyfile[d,40] <- mean(ga, na.rm = TRUE) 
+	dailyfile[d,41] <- mean(gc, na.rm = TRUE) 
+
     } 
 
     ### ----------------------------------------------------- ###
@@ -269,7 +320,12 @@ for(i in 1:length(fileList)){
 }
 
 # Finally, write the result to a single csv. Here, the row.names argument stops R from including a pesky first column of numerical row numbers.. which is of no use for us. This will be a large text file, ane may take some time to write.
-message('Writing concatenated 30 minute file', "\r", appendLF=FALSE)
-write.table(AmfluxFile, 'AllAmfluxData_30min_Reichstein.csv', sep = ',', row.names = FALSE)
+#message('Writing concatenated 30 minute file', "\r", appendLF=FALSE)
+#write.table(AmfluxFile, 'AllAmfluxData_30min_Reichstein.csv', sep = ',', row.names = FALSE)
 message('Writing concatenated daily file', "\r", appendLF=FALSE)
-write.table(AmfluxFileDaily, 'AllAmfluxData_Daily_Reichstein.csv', sep = ',', row.names = FALSE)
+write.table(AmfluxFileDaily, paste(outDir, 'AllAmfluxData_Daily_Reichstein.csv', sep = ''), sep = ',', row.names = FALSE)
+
+## end
+AmfluxFileDaily$ga[AmfluxFileDaily$ga > 2] <- NaN
+AmfluxFileDaily$ga[AmfluxFileDaily$ga < -0.1] <- NaN
+ggplot(AmfluxFileDaily, aes(DOY, ga)) + geom_point() + facet_grid(SITE~YEAR) + theme_bw()
